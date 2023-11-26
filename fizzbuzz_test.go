e17a1a6dfcce7a603afcfb42611abb6b7e944407
Sam Cranford <me@nostradamnit.com>
2023-11-26 21:36:28 +0100

    We have a failing test, CanSayFizz

--- fizzbuzz_test.go
+++ fizzbuzz_test.go
@@ -9,3 +9,11 @@ func TestCanSayOne(t *testing.T) {
 		t.Errorf("Not the right answer. We expected '1' but got '%v'", one)
 	}
 }
+
+func TestCanSayFizz(t *testing.T) {
+	fizz := fizzbuzz(3)
+
+	if fizz != "Fizz" {
+		t.Errorf("We expected 'Fizz', but got '%v'", fizz)
+	}
+}
